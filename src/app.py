0d728731275a2051f1cc2fbbd5086baf3bc208a4
Ottapiens <Ottapiens@users.noreply.github.com>
2025-10-01 14:58:38 +0700

    Add security bug

--- src/app.py
+++ src/app.py
@@ -5,3 +5,10 @@ def divide(a, b):
     if b == 0:
         raise ValueError("Tidak boleh bagi nol")
     return a / b
+
+import subprocess
+
+def run_command(cmd):
+    # BUG: penggunaan shell=True berbahaya (Command Injection)
+    result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
+    return result.stdout
